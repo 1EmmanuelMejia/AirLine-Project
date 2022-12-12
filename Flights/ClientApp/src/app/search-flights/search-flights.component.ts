cd94ff4b5d8a07fae6509188f5cb9ac8cbe144d8
DESKTOP-0BLG8KK\1Emma <1emmanuel.m.29@gmail.com>
2022-12-11 21:41:35 -0500

    added loops to test output of data

--- Flights/ClientApp/src/app/search-flights/search-flights.component.ts
+++ Flights/ClientApp/src/app/search-flights/search-flights.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './search-flights.component.html',
   styleUrls: ['./search-flights.component.css']
 })
-export class SearchFlightsComponent {
 
+ 
+export class SearchFlightsComponent
+{
+  //we are adding an array of strings here to display the airlines
+  searchResult: any = [
+
+    "American Airlines",
+    "Jet Blue",
+    "Delta"
+
+    ]
 }
